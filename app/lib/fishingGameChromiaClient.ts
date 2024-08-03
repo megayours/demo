a9551304fd6bc641784f65c48b222083c5fddae9
Viktor Plane <viktor@planevin.se>
2024-08-03 15:21:31 +0200

    Update BRID used for Fishing Game

--- app/lib/fishingGameChromiaClient.ts
+++ app/lib/fishingGameChromiaClient.ts
@@ -2,7 +2,7 @@ import { createClient, IClient } from "postchain-client";
 
 // Configuration can be set via environment variables
 const NODE_URL_POOL = process.env.NEXT_PUBLIC_D1_NODE_URL_POOL || "http://localhost:7740";
-const BLOCKCHAIN_RID = process.env.NEXT_PUBLIC_MEGA_CHAIN_BLOCKCHAIN_RID;
+const BLOCKCHAIN_RID = process.env.NEXT_PUBLIC_GAME_CHAIN_BLOCKCHAIN_RID;
 
 // Initialize the blockchain client
 let fishingGameChromiaClient: IClient;
